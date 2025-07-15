290fdb1a9ce249b069b7b253e5260c36cb5126c6
Arifin Depay Jr <finndantonio@gmail.com>
2025-07-15 07:06:58 +0700

    initilize some part

--- db.py
+++ db.py
@@ -5,8 +5,8 @@ def get_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST", "localhost"),
         user=os.getenv("DB_USER", "root"),
-        password=os.getenv("DB_PASS"),
-        database=os.getenv("DB_NAME"),
+        password=os.getenv("DB_PASS" , "@@Arifin012"),
+        database=os.getenv("DB_NAME" , "Automata"),
         auth_plugin="mysql_native_password",
         autocommit=True,
     )
